d1dc523d47e58f19d4cb593df6b7ee8f1669234c
P Clancy <P_Clancy@PClancy.com>
2011-03-28 21:34:28 -0400

    This is an example of nested tags..Doesn't really work wuite right

--- ead.py
+++ ead.py
@@ -14,25 +14,57 @@ class TagReport(object):
 
     def __init__(self):
         self.tag_dict = {}
-
+    
     def parse_file(self, filename):
-        self.parse_c02(filename)
+        self.parse_eadid_countrycode(filename)
+        self.parse_titlestmt_subtittle(filename)
+        self.parse_frontmatter_subtitle(filename)
+        self.parse_titlestmt_author(filename)
 
     def display_report(self):
         print "\n==========   Tag Report  ============\n"
         for k, v in self.tag_dict.iteritems():
             print '%s: %s' % (k, v)
         print "\n========   End Tag Report  ==========="
-        print "\n\n\n\n\n\nLongCat is l%sng" % ''.join(['o' for r in range(1000)])
-
-    def parse_c02(self, filename):
+        
+             
+    
+            
+    def parse_eadid_countrycode(self, filename):
         f = open(filename, 'r')
-        results = re.findall(r'\<c02\>|\</c02>', f.read())
+        results = re.findall(r'(\<eadid.*countrycode.*\>)', f.read())
         f.close()
-        if 'c02' in self.tag_dict:
-            self.tag_dict['c02'] = tag_dict['c02'] + len(results)
+        if 'eadid_countrycode' in self.tag_dict:
+            self.tag_dict['eadid_countrycode'] = self.tag_dict['eadid_countrycode'] + len(results)
         else:
-            self.tag_dict['c02'] = len(results)
+            self.tag_dict['eadid_countrycode'] = len(results)            
+
+    def parse_titlestmt_subtittle(self, filename):
+        f = open(filename, 'r')
+        results = re.findall(r'\<eadheader.*\<titlestmt.*\<subtittle.*\>', f.read())
+        f.close()
+        if 'titlestmt_subtittle' in self.tag_dict:
+            self.tag_dict['titlestmt_subtittle'] = self.tag_dict['titlestmt_subtittle'] + len(results)
+        else:
+            self.tag_dict['titlestmt_subtittle'] = len(results)
+
+    def parse_frontmatter_subtitle(self, filename):
+        f = open(filename, 'r')
+        results = re.findall(r'\<eadheader.*\<frontmatter.*\<subtittle.*\>', f.read())
+        f.close()
+        if 'frontmatter_subtitle' in self.tag_dict:
+            self.tag_dict['frontmatter_subtitle'] = self.tag_dict['frontmatter_subtitle'] + len(results)
+        else:
+            self.tag_dict['subtittle'] = len(results)
+            
+    def parse_titlestmt_author(self, filename):
+        f = open(filename, 'r')
+        results = re.findall(r'\<eadheader.*\<titlestmt.*\<author.*\</author>', f.read())
+        f.close()
+        if 'titlestmt_author' in self.tag_dict:
+            self.tag_dict['titlestmt_author'] = self.tag_dict['titlestmt_author'] + len(results)
+        else:
+            self.tag_dict['titlestmt_author'] = len(results)
 
 if __name__=='__main__':
    main()
