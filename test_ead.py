c68d6e5984aea3fd76835f0b045a1ff99fa0ad4f
bmbouter <bmbouter@gmail.com>
2011-06-13 22:23:47 -0400

    Added an example of a true negative test for eadid_country_code

--- test_ead.py
+++ test_ead.py
@@ -8,12 +8,19 @@ class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
         self.report = TagReport()
 
-    def test_count_eadid_countrycode(self):
+    def test_count_eadid_countrycode_true_positive(self):
         data = """<eadid countrycode="us" encodinganalog="identifier" mainagencycode="mtg"
                   identifier="80444/xv66365"
                   url="http://nwda-db.wsulibs.wsu.edu/findaid/ark:/80444/xv66365">MTGMss505.xml</eadid>"""
         self.report.count_eadid_countrycode(data)
         self.assertEqual(self.report.tag_dict, {'eadid_countrycode': 1})
+
+    def test_count_eadid_countrycode_true_negative(self):
+        data = """<eadid encodinganalog="identifier" mainagencycode="mtg"
+                  identifier="80444/xv66365"
+                  url="http://nwda-db.wsulibs.wsu.edu/findaid/ark:/80444/xv66365">MTGMss505.xml</eadid>"""
+        self.report.count_eadid_countrycode(data)
+        self.assertEqual(self.report.tag_dict, {'eadid_countrycode': 0})
 
 if __name__ == '__main__':
     unittest.main()
