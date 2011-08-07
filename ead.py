911505bdf8c696209f1fb8c9d3fe54f5e0d9c376
P Clancy <P_Clancy@PClancy.com>
2011-08-07 19:43:04 -0400

    Added re.DOTALL to Count functionsfor: titlestmt_subtitle, titlepage_subtitle, titlestmt_titleproper, titlepage_titleproper, titlepage_publisher, repository_address, publicationstmt_date, titleproper_date, creation_date, frontmatter_date, langmaterial_language, langusage_language, origination_corpname, origination_famname, origination_persname, repository_subarea

--- ead.py
+++ ead.py
@@ -39,7 +39,6 @@ class TagReport(object):
         self.count_publicationstmt_date(the_data)
         self.count_titleproper_date(the_data)
         self.count_creation_date(the_data)
-        self.count_titlestmt_subtitle_date(the_data)
         self.count_frontmatter_date(the_data)
         self.count_titlepage_subtitle_date(the_data)
         self.count_langmaterial_language(the_data)
@@ -262,14 +261,14 @@ class TagReport(object):
             self.tag_dict['unitid_countrycode'] = len(results)
 
     def count_titlestmt_subtitle(self, the_data):
-        results = re.findall(r'\<eadheader.*?\<titlestmt.*?\<subtitle.*?\>.*?\</eadheader.*?\>', the_data)
+        results = re.findall(r'\<eadheader.*?\<titlestmt.*?\<subtitle.*?\>.*?\</eadheader.*?\>', the_data, re.DOTALL)
         if 'titlestmt_subtitle' in self.tag_dict:
             self.tag_dict['titlestmt_subtitle'] = self.tag_dict['titlestmt_subtitle'] + len(results)
         else:
             self.tag_dict['titlestmt_subtitle'] = len(results)
 
     def count_titlepage_subtitle(self, the_data):
-        results = re.findall(r'\<titlepage.*?\<subtitle.*?\>.*?\</titlepage.*?\>', the_data)
+        results = re.findall(r'\<titlepage.*?\<subtitle.*?\>.*?\</titlepage.*?\>', the_data, re.DOTALL)
         if 'titlepage_subtitle' in self.tag_dict:
             self.tag_dict['titlepage_subtitle'] = self.tag_dict['titlepage_subtitle'] + len(results)
         else:
@@ -297,14 +296,14 @@ class TagReport(object):
             self.tag_dict['titlepage_sponsor'] = len(results)
 
     def count_titlestmt_titleproper(self, the_data):
-        results = re.findall(r'\<eadheader.*?\<titlestmt.*?\<titleproper.*?\>.*?\</eadheader.*?\>', the_data)
+        results = re.findall(r'\<eadheader.*?\<titlestmt.*?\<titleproper.*?\>.*?\</eadheader.*?\>', the_data, re.DOTALL)
         if 'titlestmt_titleproper' in self.tag_dict:
             self.tag_dict['titlestmt_titleproper'] = self.tag_dict['titlestmt_titleproper'] + len(results)
         else:
             self.tag_dict['titlestmt_titleproper'] = len(results)
 
     def count_titlepage_titleproper(self, the_data):
-        results = re.findall(r'\<titlepage.*?\<titleproper.*?\>.*?\</titlepage.*?\>', the_data)
+        results = re.findall(r'\<titlepage.*?\<titleproper.*?\>.*?\</titlepage.*?\>', the_data, re.DOTALL)
         if 'titlepage_titleproper' in self.tag_dict:
             self.tag_dict['titlepage_titleproper'] = self.tag_dict['titlepage_titleproper'] + len(results)
         else:
@@ -325,7 +324,7 @@ class TagReport(object):
             self.tag_dict['publicationstmt_publisher'] = len(results)
 
     def count_titlepage_publisher(self, the_data):
-        results = re.findall(r'\<titlepage.*?\<publisher.*?\>*?\</titlepage.*?\>', the_data)
+        results = re.findall(r'\<titlepage.*?\<publisher.*?\>*?\</titlepage.*?\>', the_data, re.DOTALL)
         if 'titlepage_publisher' in self.tag_dict:
             self.tag_dict['titlepage_publisher'] = self.tag_dict['titlepage_publisher'] + len(results)
         else:
@@ -339,84 +338,77 @@ class TagReport(object):
             self.tag_dict['publicationstmt_address'] = len(results)
 
     def count_repository_address(self, the_data):
-        results = re.findall(r'\<did.*?\<repository.*?\<address.*?\>.*?\</did.*?\>', the_data)
+        results = re.findall(r'\<did.*?\<repository.*?\<address.*?\>.*?\</did.*?\>', the_data, re.DOTALL)
         if 'repository_address' in self.tag_dict:
             self.tag_dict['repository_address'] = self.tag_dict['repository_address'] + len(results)
         else:
             self.tag_dict['repository_address'] = len(results)
 
     def count_publicationstmt_date(self, the_data):
-        results = re.findall(r'\<filedesc.*?\<publicationstmt.*?\<date.*?\>.*?\</filedesc.*?\>', the_data)
+        results = re.findall(r'\<filedesc.*?\<publicationstmt.*?\<date.*?\>.*?\</filedesc.*?\>', the_data, re.DOTALL)
         if 'publicationstmt_date' in self.tag_dict:
             self.tag_dict['publicationstmt_date'] = self.tag_dict['publicationstmt_date'] + len(results)
         else:
             self.tag_dict['publicationstmt_date'] = len(results)
 
     def count_titleproper_date(self, the_data):
-        results = re.findall(r'\<titlestmt.*?\<titleproper.*?\<date.*?\>.*?\</titlestmt\>', the_data)
+        results = re.findall(r'\<titleproper.*?\<date.*?\>.*?\</titleproper.*?\>', the_data, re.DOTALL)
         if 'titleproper_date' in self.tag_dict:
             self.tag_dict['titleproper_date'] = self.tag_dict['titleproper_date'] + len(results)
         else:
             self.tag_dict['titleproper_date'] = len(results)
 
     def count_creation_date(self, the_data):
-        results = re.findall(r'\<profiledesc.*?\<creation.*?\<date.*?\>.*?\</profiledesc\>', the_data)
+        results = re.findall(r'\<profiledesc.*?\<creation.*?\<date.*?\>.*?\</profiledesc\>', the_data, re.DOTALL)
         if 'creation_date' in self.tag_dict:
             self.tag_dict['creation_date'] = self.tag_dict['creation_date'] + len(results)
         else:
             self.tag_dict['creation_date'] = len(results)
             
-    def count_titlestmt_subtitle_date(self, the_data):
-        results = re.findall(r'\<titlestmt.*?\<subtitle.*?\<date.*?\>.*?\</titlestmt\>', the_data)
-        if 'titlestmt_subtitle_date' in self.tag_dict:
-            self.tag_dict['titlestmt_subtitle_date'] = self.tag_dict['titlestmt_subtitle_date'] + len(results)
-        else:
-            self.tag_dict['titlestmt_subtitle_date'] = len(results)
-
     def count_frontmatter_date(self, the_data):
-        results = re.findall(r'\<frontmatter.*?\<titlepage.*?\<date.*?\>.*?\</frontmatter\>', the_data)
+        results = re.findall(r'\<frontmatter.*?\<date.*?\>.*?\</frontmatter\>', the_data, re.DOTALL)
         if 'frontmatter_date' in self.tag_dict:
             self.tag_dict['frontmatter_date'] = self.tag_dict['frontmatter_date'] + len(results)
         else:
             self.tag_dict['frontmatter_date'] = len(results)
   
     def count_titlepage_subtitle_date(self, the_data):
-        results = re.findall(r'\<titlepage.*?\<subtitle.*?\<date.*?\>.*?\</titlepage\>', the_data)
+        results = re.findall(r'\<titlepage.*?\<subtitle.*?\<date.*?\>.*?\</titlepage\>', the_data, re.DOTALL)
         if 'titlepage_subtitle_date' in self.tag_dict:
             self.tag_dict['titlepage_subtitle_date'] = self.tag_dict['titlepage_subtitle_date'] + len(results)
         else:
             self.tag_dict['titlepage_subtitle_date'] = len(results)
 
     def count_langmaterial_language(self, the_data):
-        results = re.findall(r'<langmaterial.*?\<language.*?\>.*?\</langmaterial\>', the_data)
+        results = re.findall(r'<langmaterial.*?\<language.*?\>.*?\</langmaterial\>', the_data, re.DOTALL)
         if 'langmaterial_language' in self.tag_dict:
             self.tag_dict['langmaterial_language'] = self.tag_dict['langmaterial_language'] + len(results)
         else:
             self.tag_dict['langmaterial_language'] = len(results)
             
     def count_langusage_language(self, the_data):
-        results = re.findall(r'\<profiledesc.*?\<langusage.*?\<language.*?\>.*?\</profiledesc\>', the_data)
+        results = re.findall(r'\<profiledesc.*?\<langusage.*?\<language.*?\>.*?\</profiledesc\>', the_data, re.DOTALL)
         if 'langusage_language' in self.tag_dict:
             self.tag_dict['langusage_language'] = self.tag_dict['langusage_language'] + len(results)
         else:
             self.tag_dict['langusage_language'] = len(results)            
     
     def count_origination_corpname(self, the_data):
-        results = re.findall(r'\<origination.*?\<corpname.*?\</corpname\>', the_data)
+        results = re.findall(r'\<origination.*?\<corpname.*?\</corpname\>', the_data, re.DOTALL)
         if 'origination_corpname' in self.tag_dict:
             self.tag_dict['origination_corpname'] = self.tag_dict['origination_corpname'] + len(results)
         else:
             self.tag_dict['origination_corpname'] = len(results)              
 
     def count_origination_famname(self, the_data):
-        results = re.findall(r'\<origination.*?\<famname.*?\</famname\>', the_data)
+        results = re.findall(r'\<origination.*?\<famname.*?\</famname\>', the_data, re.DOTALL)
         if 'origination_famname' in self.tag_dict:
             self.tag_dict['origination_famname'] = self.tag_dict['origination_famname'] + len(results)
         else:
             self.tag_dict['origination_famname'] = len(results)
     
     def count_origination_persname(self, the_data):
-        results = re.findall(r'\<origination.*?\<persname.*?\</persname\>', the_data)
+        results = re.findall(r'\<origination.*?\<persname.*?\</persname\>', the_data, re.DOTALL)
         if 'origination_persname' in self.tag_dict:
             self.tag_dict['origination_persname'] = self.tag_dict['origination_persname'] + len(results)
         else:
@@ -430,7 +422,7 @@ class TagReport(object):
             self.tag_dict['repository_corpname'] = len(results)
 
     def count_repository_subarea(self, the_data):
-        results = re.findall(r'\<repository.*?\<subarea.*?\</repository\>', the_data)
+        results = re.findall(r'\<repository.*?\<subarea.*?\</repository\>', the_data, re.DOTALL)
         if 'repository_subarea' in self.tag_dict:
             self.tag_dict['repository_subarea'] = self.tag_dict['repository_subarea'] + len(results)
         else:
